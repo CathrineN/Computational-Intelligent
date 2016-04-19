f7145a0a83e452364f55358bd9a885e72ff85b95
Cathrine Nicea <cathrine.nicea@gmail.com>
2016-04-18 22:04:05 -0700

    Tugas MID Computational Intelligent
    Program Aplikasi "Algoritma Genetik untuk menyelesaikan Masalah pada Knapsack Problem [1,0]" dan Review Jurnal "Computer-Aided Diagnosis (CAD) Of Breast Cancer Using Artificial Neural Networks : Comparison of Backpropagation and Genetic Algorithms"

---
+++ KnapSackProblem/KnapsackProblem.dpr
@@ -0,0 +1,14 @@
+program KnapsackProblem;
+
+uses
+  Forms,
+  Knapsack in 'Knapsack.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
